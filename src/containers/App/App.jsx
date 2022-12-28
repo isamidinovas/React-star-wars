2b8414d3546048a1c47d82eb2866c14d56db2634
isamidinovas <99507186+isamidinovasymbatt@users.noreply.github.com>
2022-12-28 20:59:29 +0600

    added a function for requests to the server

---
+++ src/containers/App/App.jsx
@@ -0,0 +1,8 @@
+import styles from "./App.module.css";
+import { getApiResource } from "../../utils/network";
+
+const App = () => {
+  return <h1 className={styles.header}>React</h1>;
+};
+
+export default App;
