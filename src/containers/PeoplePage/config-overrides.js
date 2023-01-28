101590decc6322569b4a691857eb92d9bfb3e74a
isamidinovas <99507186+isamidinovasymbatt@users.noreply.github.com>
2023-01-28 17:28:43 +0600

    I tried Alias but it doesn't work.

---
+++ src/containers/PeoplePage/config-overrides.js
@@ -0,0 +1,12 @@
+// const { alias } = require("react-app-rewire-alias");
+
+// module.exports = function override(config, env) {
+//   alias({
+//     "@components": "src/components",
+//     "@constants": "src/constants",
+//     "@hoc-helper": "src/hoc-helper",
+//     "@services": "src/services",
+//     "@utils": "src/utils",
+//   })(config);
+//   return config;
+// };
