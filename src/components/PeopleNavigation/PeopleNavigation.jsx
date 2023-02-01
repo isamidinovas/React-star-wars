4b5ac15bc7934ddf373fd380a78aacc4abf0f8fa
isamidinovas <99507186+isamidinovasymbatt@users.noreply.github.com>
2023-02-01 11:49:26 +0600

    added classnames labr. and created Ui Button

--- src/components/PeopleNavigation/PeopleNavigation.jsx
+++ src/components/PeopleNavigation/PeopleNavigation.jsx
@@ -1,30 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import UiButton from "../Ui/UiButton";
 import styles from "./PeopleNavigation.module.css";
+
 const PeopleNavigation = ({ getResource, prevPage, nextPage, counterPage }) => {
   const handleChangeNext = () => getResource(nextPage);
   const handleChangePrev = () => getResource(prevPage);
 
   return (
-    <div>
-      <Link to={`/people/?page=${counterPage - 1}`} className={styles.link}>
-        <button
+    <div className={styles.container}>
+      <Link to={`/people/?page=${counterPage - 1}`} className={styles.buttons}>
+        <UiButton
+          text="Previous"
           onClick={handleChangePrev}
-          className={styles.buttons}
           disabled={!prevPage}
-        >
-          Previous
-        </button>
+        />
       </Link>
-      <Link to={`/people/?page=${counterPage + 1}`} className={styles.link}>
-        <button
-          onClick={handleChangeNext}
-          className={styles.buttons}
-          disabled={!nextPage}
-        >
-          Next
-        </button>
+      <Link to={`/people/?page=${counterPage + 1}`} className={styles.buttons}>
+        <UiButton text="Next" onClick={handleChangeNext} disabled={!nextPage} />
       </Link>
     </div>
   );
